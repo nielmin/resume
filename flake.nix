2252e68e00b3b8cfb3e828f215928a3f04579365
Daniel Hwang <dawh@ines.home>
2026-04-27 10:50:32 -0500

    Add numtide/devshell to flake.nix

--- flake.nix
+++ flake.nix
@@ -2,17 +2,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;}
+  outputs = inputs @ { devshell, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; }
     {
-      systems = ["x86_64-linux"];
-      perSystem = {pkgs, ...}: {
-        devShells.default = pkgs.mkShell {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, ... }: {
+        devshells.default = {
+          commands = [
+            {
+              name  = "compile";
+              command = "typst compile main.typ";
+              help = "compiles resume to pdf";
+            }
+          ];
           packages = with pkgs; [
             typst
-           typstyle
+            typstyle
           ];
         };
 
